201b675c2fcfc3b843d35dc84822a974dbfb49bc
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-14 20:58:55 -0300

    Create 08conversor_de_R$.py

---
+++ 08conversor_de_R$.py
@@ -0,0 +1,4 @@
+# exercício 8: Desenvolva um programa que leia quanto dinheiro uma pessoa tem e mostre quantos dólares ela pode comprar
+
+val = float(input('Valor de din: R$'))
+print('Você pode comprar ${:.2f}'.format(val/4.92))
