044c04e030abdfac2d03d537434282d67465c240
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-14 20:56:54 -0300

    Create 05media.py

---
+++ 05media.py
@@ -0,0 +1,5 @@
+# exercício 5: Desenvolva um programa que leia as duas notas de um aluno. Calcule e mostra a média
+
+n1 = float(input('Nota 1: '))
+n2 = float(input('Nota 2: '))
+print('A média do aluno é: {} '. format((n1+n2)/2))
